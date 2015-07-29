84d782d88a6f8bf95e47787894353e37670d7e83
Nick Martin <me@nickmartin.us>
2015-07-29 08:08:56 -0500

    Working on getting react in place

--- src/app.js
+++ src/app.js
@@ -1,42 +1,50 @@
-import 'bootstrap';
-import 'bootstrap/css/bootstrap.css!';
+import {
+    Router
+}
+from 'aurelia-router';
 
 var base = './';
 
 export class App {
-    configureRouter(config, router) {
-        config.title = 'FBTB';
-        config.map([
-            {
-                route: ['', 'news'],
-                name: 'news',
-                moduleId: base + 'news/news',
-                nav: true,
-                title: 'News'
-            },
-            {
-                route: 'guide',
-                name: 'guide',
-                moduleId: base + 'guide/guide',
-                nav: true,
-                title: 'Flickr'
-            },
-            {
-                route: 'flickr',
-                name: 'flickr',
-                moduleId: 'flickr/flickr',
-                nav: true,
-                title: 'Flickr'
-            },
-            {
-                route: 'child-router',
-                name: 'child-router',
-                moduleId: base + 'demo/demo',
-                nav: true,
-                title: 'Child Router'
-            }
-    ]);
-
+    static inject() {
+        return [Router];
+    }
+    constructor(router) {
         this.router = router;
+        this.router.configure(config => {
+            config.title = 'FBTB';
+            config.map([
+                { route: ['', 'articles'], name: 'news', moduleId: base + 'news/news', nav: true, title: 'Articles' },
+                { route: 'articles/type', name: 'reviews', moduleId: base + 'news/news', nav: true, title: 'Reviews' },
+                {
+                    route: 'guide',
+                    name: 'guide',
+                    moduleId: base + 'guide/guide',
+                    nav: true,
+                    title: 'Guide'
+            },
+                {
+                    route: 'community',
+                    name: 'community',
+                    moduleId: 'communit/communit',
+                    nav: true,
+                    title: 'Community'
+            },
+                {
+                    route: 'chat',
+                    name: 'chat',
+                    moduleId: 'chat/chat',
+                    nav: true,
+                    title: 'Chat'
+            },
+                {
+                    route: 'child-router',
+                    name: 'child-router',
+                    moduleId: base + 'demo/demo',
+                    nav: false,
+                    title: 'Child Router'
+            }
+          ]);
+        })
     }
 }
--- src/news/news.js
+++ src/news/news.js
@@ -1,33 +1,35 @@
-import {computedFrom} from 'aurelia-framework';
+import { computedFrom } from 'aurelia-framework';
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-http-client';
 
-export class Welcome{
-  heading = 'Welcome to the Aurelia Navigation App!';
-  firstName = 'John';
-  lastName = 'Doe';
-  previousValue = this.fullName;
+@inject(HttpClient)
+export class News {
+    heading = 'From Bricks to Bothans';
+    url = 'http://fbtb.dwhisper.com/wp-json/posts';
+    articles = [];
 
-  //Getters can't be observed with Object.observe, so they must be dirty checked.
-  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-  //To optimize by declaring the properties that this getter is computed from, uncomment the line below.
-  //@computedFrom('firstName', 'lastName')
-  get fullName(){
-    return `${this.firstName} ${this.lastName}`;
-  }
-
-  submit(){
-    this.previousValue = this.fullName;
-    alert(`Welcome, ${this.fullName}!`);
-  }
-
-  canDeactivate() {
-    if (this.fullName !== this.previousValue) {
-      return confirm('Are you sure you want to leave?');
+    constructor(http) {
+        this.http = http;
     }
-  }
+
+    submit() {
+        
+    }
+
+    canDeactivate() {
+        
+    }
+
+    activate(settings) {
+        return this.http.get(this.url).then(response => {
+          this.articles = response.content;
+            console.log(this.articles);
+        });
+    }
 }
 
 export class UpperValueConverter {
-  toView(value){
-    return value && value.toUpperCase();
-  }
+    toView(value) {
+        return value && value.toUpperCase();
+    }
 }
