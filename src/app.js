bf2cabfb2d2e9f53408f57ae54bce5c62b90c1a8
Nick Martin <me@nickmartin.us>
2015-08-18 00:11:00 -0500

    Playing with the styles and layout
    Trying to get different routes set up, but it’s not working yet. Also
    need to work on getting the social bar and some models configured for
    it.

--- src/app.js
+++ src/app.js
@@ -14,37 +14,12 @@ export class App {
         this.router.configure(config => {
             config.title = 'FBTB';
             config.map([
-                { route: ['', 'articles'], name: 'news', moduleId: base + 'news/news', nav: true, title: 'Articles' },
-                { route: 'articles/type', name: 'reviews', moduleId: base + 'news/news', nav: true, title: 'Reviews' },
-                { route: 'articles/type', name: 'reviews', moduleId: base + 'news/news', nav: true, title: 'Hey Buddy' },
-                {
-                    route: 'guide',
-                    name: 'guide',
-                    moduleId: base + 'guide/guide',
-                    nav: true,
-                    title: 'Guide'
-            },
-                {
-                    route: 'community',
-                    name: 'community',
-                    moduleId: 'communit/communit',
-                    nav: true,
-                    title: 'Community'
-            },
-                {
-                    route: 'chat',
-                    name: 'chat',
-                    moduleId: 'chat/chat',
-                    nav: true,
-                    title: 'Chat'
-            },
-                {
-                    route: 'child-router',
-                    name: 'child-router',
-                    moduleId: base + 'demo/demo',
-                    nav: false,
-                    title: 'Child Router'
-            }
+                { route: ['', 'articles'], name: 'news', moduleId: base + 'news/news', nav: true, title: 'News' },
+                { route: 'articles/:type', name: 'reviews', moduleId: base + 'news/news', nav: true, title: 'Reviews' },
+                { route: 'article/:type', name: 'news', moduleId: base + 'news/news', nav: true, title: 'Articles' },
+                { route: 'guide', name: 'guide', moduleId: base + 'guide/guide', nav: true, title: 'Guide' },
+                { route: 'community', name: 'community', moduleId: 'communit/communit', nav: true, title: 'Community' },
+                { route: 'child-router', name: 'child-router', moduleId: base + 'demo/demo', nav: false, title: 'Child Router' }
           ]);
         })
     }
